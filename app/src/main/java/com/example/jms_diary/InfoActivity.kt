bedec923cac08fdfe286f880f5bb2780293f21fc
Chaejongin12 <s22055@gsm.hs.kr>
2023-11-23 03:58:40 +0900

    코드 설명주석 추가

--- app/src/main/java/com/example/jms_diary/InfoActivity.kt
+++ app/src/main/java/com/example/jms_diary/InfoActivity.kt
@@ -1,9 +1,8 @@
-package com.example.jms_diary
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.example.jms_diary.R
 import com.example.jms_diary.databinding.ActivityInfoBinding
 
 class InfoActivity : AppCompatActivity() {
@@ -12,13 +11,17 @@ class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // DataBinding을 설정하고, layout 파일을 기반으로 binding 객체를 초기화
         binding = DataBindingUtil.setContentView(this, R.layout.activity_info)
 
+        // MainActivity에서 전달한 데이터를 Intent에서 추출
         val diaryTitle = intent.getStringExtra("diaryTitle")
         val diaryImage = intent.getStringExtra("diaryImage")
         val diaryContent = intent.getStringExtra("diaryContent")
 
+        // 레이아웃에 데이터를 표시
         binding.diaryTitle.text = diaryTitle
+        // 이미지 로딩 라이브러리인 Glide를 사용하여 이미지를 로드하고 이미지뷰에 표시
         Glide.with(this).load(diaryImage).into(binding.image)
         binding.content.text = diaryContent
     }
--- app/src/main/java/com/example/jms_diary/MainActivity.kt
+++ app/src/main/java/com/example/jms_diary/MainActivity.kt
@@ -1,39 +1,53 @@
-package com.example.jms_diary
-
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.example.jms_diary.InfoActivity
+import com.example.jms_diary.R
 import com.example.jms_diary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // View들에 접근하기 위해 DataBinding을 사용
     private lateinit var binding: ActivityMainBinding
+    // 갤러리에서 이미지를 가져오기 위한 요청 코드
     private val GET_GALLERY_IMAGE = 200
-    private var selectedImageUri: Uri? = null // 이미지 URI를 저장할 변수
+    // 이미지 URI를 저장할 변수
+    private var selectedImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // DataBinding을 설정하고, layout 파일을 기반으로 binding 객체를 초기화
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // 이미지를 선택하기 위한 갤러리 열기
         binding.image.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
+            // 이미지 타입으로 필터링
             intent.data = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             intent.type = "image/*"
+            // 갤러리 열기를 요청하고 결과를 받기 위해 startActivityForResult 사용
             startActivityForResult(intent, GET_GALLERY_IMAGE)
         }
 
+        // 버튼을 눌렀을 때 동작하는 코드
         binding.button.setOnClickListener {
+            // 제목에 입력된 텍스트 값을 가져옴
             val diaryTitle = binding.writeDiary.text.toString()
-            val diaryImage = selectedImageUri?.toString() // 이미지 URI를 문자열로 저장
+            // 이미지 URI를 문자열로 저장
+            val diaryImage = selectedImageUri?.toString()
+            // 내용에 입력된 텍스트 값을 가져옴
             val diaryContent = binding.editText.text.toString()
 
+            // InfoActivity로 이동하면서 데이터를 넘겨주는 Intent 생성
             val intent = createInfoActivityIntent(diaryTitle, diaryImage, diaryContent)
+            // 생성한 Intent를 사용하여 새로운 Activity로 이동
             startActivity(intent)
         }
     }
 
+    // InfoActivity로 이동하면서 데이터를 넘겨주는 함수
     private fun createInfoActivityIntent(diaryTitle: String, diaryImage: String?, diaryContent: String): Intent {
         return Intent(this, InfoActivity::class.java).apply {
             putExtra("diaryTitle", diaryTitle)
@@ -42,11 +56,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // 갤러리에서 이미지를 선택한 결과를 처리하는 콜백 메서드
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // 갤러리에서 이미지를 선택한 결과이면서 성공한 경우
         if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.data != null) {
+            // 선택된 이미지의 URI를 변수에 저장
             selectedImageUri = data.data
+            // 이미지뷰에 선택된 이미지 표시
             binding.image.setImageURI(selectedImageUri)
         }
     }
